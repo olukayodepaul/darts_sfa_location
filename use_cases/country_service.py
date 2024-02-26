fc612ac6bd7fbe4d3745bb76d2b378701cc0eaa4
Paul Aigbokhai Olukayode <paulaigbokhaiolukayode@USERs-MacBook-Pro.local>
2024-02-26 14:40:58 +0100

    feat: add country crud

--- use_cases/country_service.py
+++ use_cases/country_service.py
@@ -1,3 +1,41 @@
+from sqlalchemy.orm import Session
+from data_model import sqlalchemy_model as schemas 
+from data_model.http_response import http_message, http_data, http_exception_404
+from typing import Dict
+
 
 class CountryService:
-    pass
+    
+    def __init__(self, db: Session, payload: Dict):
+        self.db = db
+        
+    def get_all_country(self):
+        
+        get_data = self.db.query(schemas.Country).all()
+        if not get_data:
+            raise http_exception_404(app_message='Country not found', app_status='Error')
+        
+        response_builder = {
+            **http_message(app_message='Country found', app_status='Successful'),
+            **http_data()
+        }
+        
+        
+        response_builder.update({"data": get_data})
+        return {"detail":response_builder}
+        
+        
+    def get_country(self, continent_id: int):
+        
+        get_data = self.db.query(schemas.Country).filter(schemas.Country.continent_id == continent_id).all()
+        
+        if not get_data:
+            raise http_exception_404(app_message='Country not found', app_status='Error')
+        
+        response_builder = {
+            **http_message(app_message='Country found', app_status='Successful'),
+            **http_data()
+        }
+        
+        response_builder.update({"data": get_data})
+        return {"detail":response_builder}
